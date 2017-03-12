a1bba0293c6c0a1e60e6d8977a92885778fb5500
David Garry <dgarry@usc.edu>
2017-03-12 15:47:59 -0700

    hello

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,6 +5,32 @@
  */
 
 
+ var step = 4, // colorChage step, use negative value to change direction
+     ms   = 100,  // loop every
+     $uni = $('.unicorn'),
+     txt  = $uni.text(),
+     len  = txt.length,
+     lev  = 360/len,
+     newCont = "",
+     itv;
+
+ for(var i=0; i<len; i++)newCont += "<span style='color:hsla("+ i*lev +", 100%, 50%, 1)'>"+ txt.charAt(i) +"</span>";
+
+ $uni.html(newCont); // Replace with new content
+ var $ch = $uni.find('span'); // character
+
+ function anim(){
+   itv = setInterval(function(){
+     $ch.each(function(){
+       var h = +$(this).attr('style').split(',')[0].split('(')[1]-step % 360;
+       $(this).attr({style:"color:hsla("+ h +", 100%, 50%, 1)"});
+     });
+   }, ms);
+ }
+ function stop(){ clearInterval(itv); }
+
+ anim();
+
 
 
 
@@ -40,8 +66,7 @@ $(function() {
 
 
 $('.navbar-collapse ul li a').click(
-    function() { 
-        $(this).closest('.collapse').collapse('toggle'); 
+    function() {
+        $(this).closest('.collapse').collapse('toggle');
     }
 );
-
